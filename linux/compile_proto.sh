1501d5e8d23aa4f41a559e0ec13eed6cba5ac51a
soheil <senisonn@gmail.com>
2025-11-03 11:09:38 +0100

    sh modif

--- linux/compile_proto.sh
+++ linux/compile_proto.sh
@@ -4,7 +4,7 @@ echo "Compilation du fichier user.proto en cours..."
 cd ..
 
 # On compile dans le dossier courant
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. user.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. user.proto
 
 if [ $? -eq 0 ]; then
     echo "   Compilation terminée avec succès !"
